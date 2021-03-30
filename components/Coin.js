4b3c7d084ea8d67081bcaf528564b59eb47bc819
Ben Cho <bycho1991@gmail.com>
2021-03-30 15:45:04 -0500

    grid to flex changes

--- components/Coin.js
+++ components/Coin.js
@@ -1,19 +1,18 @@
 
-
 const Coin = ({data}) => {
 	return (
-		<div className='w-full max-w-[1080px] grid grid-cols-4 gap-2 border-b-[1px] border-gray-400 h-[80px] flex items-center hover:bg-gray-200'>
-			<div className="name flex items-center">
+		<div className='w-full max-w-[1080px] border-b-[1px] border-gray-400 h-[80px] flex items-center hover:bg-gray-200 justify-between'>
+			<div className="name flex items-center w-1/4">
 				<img src={data.image} alt={data.name} className='w-[30px] h-[30px] mr-4'/>
 				<h1 className='text-2xl'>{data.name}</h1>
 				<p className='uppercase ml-1 text-gray-400'>{data.symbol}</p>
 			</div>
 
-			<div className="price text-right">
+			<div className="price text-right w-1/4">
 				${data.current_price.toLocaleString()}
 			</div>
 
-			<div className="percentChange text-right">
+			<div className="percentChange text-right w-1/4">
 				{data.price_change_percentage_24h < 0 ? (
 					<p className="text-red-400">{data.price_change_percentage_24h.toFixed(2)}%</p>
 				) : (
@@ -21,7 +20,7 @@ const Coin = ({data}) => {
 				)}
 			</div>
 
-			<div className="marketCap text-right">
+			<div className="marketCap text-right w-1/4">
 				${data.market_cap.toLocaleString()}
 			</div>
 		</div>
