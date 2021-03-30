94ae372700ab0b1d1783206f58cc9aaeeeace7b6
Ben Cho <bycho1991@gmail.com>
2021-03-30 11:06:55 -0500

    test commit

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+
+
+const contact = () => {
+	return (
+		<div>
+			<h1>Please contact me</h1>
+		</div>
+	)
+}
+
+export default contact
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>CryptoCurrent</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <Link href='/contact'>
+            <a>Click here to contact</a>
+          </Link>
         </p>
       </main>
 
